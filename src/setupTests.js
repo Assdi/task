4279ad2e18a19272731bd5b21262e0f8720b96a0
Asad <asad_uetians@yahoo.com>
2025-03-18 23:56:37 +0500

    #PROMPT #MODEL_OUTPUT #TEST_FIX
    Prompt Category:
    Unit Test Writing
    Prompt:
    im getting following error while running the test command please fix configurations
    Out of the box, Create React App only supports overriding these Jest options:
    • clearMocks
    • collectCoverageFrom
    • coveragePathIgnorePatterns
    • coverageReporters
    • coverageThreshold
    • displayName
    • extraGlobals
    • globalSetup
    • globalTeardown
    • moduleNameMapper
    • resetMocks
    • resetModules
    • restoreMocks
    • snapshotSerializers
    • testMatch
    • transform
    • transformIgnorePatterns
    • watchPathIgnorePatterns.
    These options in your package.json Jest configuration are not currently supported by Create React App:
    • testEnvironment
    If you wish to override other Jest options, you need to eject from the default setup. You can do so by running npm run eject but remember that this is a one-way operation. You may also file an issue with Create React App to discuss supporting more options out of the box.
    
    User Observation:
    The response fixes the configuration issue but test is failing due to module not found
    
    Response ID:
    57f6dbbe-511d-4b18-9f9f-0af4e4fb9c93

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,2 +1,20 @@
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 import '@testing-library/jest-dom';
+
+// Configure testing environment
+window.matchMedia = window.matchMedia || function() {
+  return {
+    matches: false,
+    addListener: function() {},
+    removeListener: function() {}
+  };
+};
+
+// Mock IntersectionObserver
+const mockIntersectionObserver = jest.fn();
+mockIntersectionObserver.mockReturnValue({
+  observe: () => null,
+  unobserve: () => null,
+  disconnect: () => null
+});
+window.IntersectionObserver = mockIntersectionObserver;
